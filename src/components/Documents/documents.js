e172a6dbd2f31e5a0df53c0bd84dad6d536cc196
Rahul Nag <rahulnag625@gmail.com>
2021-12-19 02:11:11 +0530

    home page done, testimonial added, documents strucure created

---
+++ src/components/Documents/documents.js
@@ -0,0 +1,70 @@
+export let documents = [
+  {
+    coursename: "Java",
+    material_link: "",
+  },
+  {
+    coursename: "Java",
+    material_link: "",
+  },
+  {
+    coursename: "Java",
+    material_link: "",
+  },
+  {
+    coursename: "Java",
+    material_link: "",
+  },
+  {
+    coursename: "Java",
+    material_link: "",
+  },
+  {
+    coursename: "Java",
+    material_link: "",
+  },
+  {
+    coursename: "Java",
+    material_link: "",
+  },
+  {
+    coursename: "Java",
+    material_link: "",
+  },
+  {
+    coursename: "Java",
+    material_link: "",
+  },
+  {
+    coursename: "Java",
+    material_link: "",
+  },
+  {
+    coursename: "Java",
+    material_link: "",
+  },
+  {
+    coursename: "Java",
+    material_link: "",
+  },
+  {
+    coursename: "Java",
+    material_link: "",
+  },
+  {
+    coursename: "Java",
+    material_link: "",
+  },
+  {
+    coursename: "Java",
+    material_link: "",
+  },
+  {
+    coursename: "Java",
+    material_link: "",
+  },
+  {
+    coursename: "Java",
+    material_link: "",
+  },
+];
